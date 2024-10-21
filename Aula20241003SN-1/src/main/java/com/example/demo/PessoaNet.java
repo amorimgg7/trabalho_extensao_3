75eb855db398c9659354df067a6ca41e9e4a51d2
amorimgg7 <amorimgg7@gmail.com>
2024-10-21 17:08:40 -0300

    Alteração na aplicação, tem que mexer na api para resolver a consulta obter(codigo)

--- Aula20241003SN-1/src/main/java/com/example/demo/PessoaNet.java
+++ Aula20241003SN-1/src/main/java/com/example/demo/PessoaNet.java
@@ -7,14 +7,15 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 //import retrofit2.http.Path;
+import retrofit2.http.Path;
 
 public interface PessoaNet {
     
-    //@GET("pessoa/{codigo}")
-    //Call<Pessoa> obter(@Path(value = "codigo") Double codigo);
+    @GET("pessoa/{codigo}")
+    Call<Pessoa> obter(@Path(value = "codigo") Integer codigo);
     
-    @GET("pessoa")
-    Call<List<Pessoa>> obter(Integer codigo);
+    //@GET("pessoa")
+    //Call<List<Pessoa>> obter(Integer codigo);
     
     
     @GET("pessoa")
